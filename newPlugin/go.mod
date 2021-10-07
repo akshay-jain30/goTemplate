6cd0d9b3ce0a641d1969bdd85a4e11b57f9e726c
Akshay Jain <akshayjain@NOVI-C02FFBR4MD6M.local>
2021-10-07 18:26:50 +0530

    create so file with new verison

---
+++ newPlugin/go.mod
@@ -0,0 +1,7 @@
+module newPlugin
+
+go 1.17
+
+require (
+	github.com/akshay-jain30/goTemplate/structs v0.0.0-20211007124049-0297b3ba6cdf
+)
---
+++ newPlugin/newPlugin.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	common "github.com/akshay-jain30/goTemplate/structs"
+)
+func returnCommonStruct() common.SampleStruct {
+	return common.SampleStruct{
+		Field1: 1,
+		Field2: 2,
+		Field3: 3,
+	}
+}
